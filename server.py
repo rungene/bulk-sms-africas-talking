3d8aa06fbaec38a4d3eedec87d26f29e57970f1f
rungene <rungenesamuel@gmail.com>
2024-05-14 08:54:09 +0000

    Resolving pycodestyle errors. Update response message

--- server.py
+++ server.py
@@ -28,22 +28,26 @@ url = 'https://api.sandbox.africastalking.com/version1/messaging'
 username = os.getenv('username')
 file_name = 'sample.csv'
 phone_numbers = read_kenyan_phone_numbers(file_name)
-message = "Welcome to Toppline Kenya"
+message = "Welcome to Toppline Kenya. Welcome to our monthly offers"
 myShortCode = 'Toppline Kenya'
+
 
 # @app.route('/sms/send',  methods=['POST', 'GET'])
 def send_sms(phone_numbers, message):
     """
-    Sends an SMS message to a list of phone numbers using the Africa's Talking API.
-    
+    Sends an SMS message to a list of phone numbers using the
+    Africa's Talking API.
+
     Args:
-        phone_numbers (list): A list of phone numbers (strings) to send the SMS to.
+        phone_numbers (list): A list of phone numbers (strings)
+        to send the SMS to.
         message (str): The message content to be sent.
 
     Return:
         None: This function does not return any value.
     Raises:
-        Exception: If the SMS sending fails due to an error response from the API.
+        Exception: If the SMS sending fails due to an error
+        response from the API.
     """
     response = requests.post(
         url,
@@ -59,13 +63,16 @@ def send_sms(phone_numbers, message):
             'apiKey': api_key
         }
     )
+    message_sent = 0
     if response.status_code == 201:
         data = response.json()
-        print(f'Received data {data}')
         response_list = data['SMSMessageData']['Recipients']
+        response_message = data['SMSMessageData']['Message']
+        print(response_message)
         for r in response_list:
             if r.get('statusCode') == 101:
-                print('message sent success fully')
+                message_sent += 1
+        print(f'{message_sent} messages sent successfully')
     else:
         print(f'Error: {response.status_code}')
 
